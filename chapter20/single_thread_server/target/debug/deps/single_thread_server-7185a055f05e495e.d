ffeefcffd49e34e7ed08597222484f1498164c1d
Henri E. Francois <henrifrancois97@gmail.com>
2018-12-26 00:29:37 -0500

    started web server project

---
+++ chapter20/single_thread_server/target/debug/deps/single_thread_server-7185a055f05e495e.d
@@ -0,0 +1,5 @@
+/home/henri/repositories/tutorial/chapter20/single_thread_server/target/debug/deps/single_thread_server-7185a055f05e495e: src/main.rs
+
+/home/henri/repositories/tutorial/chapter20/single_thread_server/target/debug/deps/single_thread_server-7185a055f05e495e.d: src/main.rs
+
+src/main.rs:
---
+++ chapter20/single_thread_server/target/debug/single_thread_server.d
@@ -0,0 +1,1 @@
+/home/henri/repositories/tutorial/chapter20/single_thread_server/target/debug/single_thread_server: /home/henri/repositories/tutorial/chapter20/single_thread_server/src/main.rs
